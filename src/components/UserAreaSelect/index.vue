77d23e079a22dabd245a5d555d784d3376b74dd0
杨可可 <yangkeke@dmtop1.com>
2023-12-17 13:31:10 +0800

    实现userAreaSelect

--- src/components/UserAreaSelect/index.vue
+++ src/components/UserAreaSelect/index.vue
@@ -23,7 +23,51 @@ export default {
   },
   computed: {
     allAreas() {
-      return this.$store.state.baseInfo.allAreas
+      const allAreasMap = this.$store.state.baseInfo.allAreasMap
+      const userArea = JSON.parse(this.$store.state.user.user.userArea)
+
+      if (Object.keys(allAreasMap).length === 0) {
+        return []
+      }
+
+      const userAreas = []
+      const userAreasMap = {}
+
+      const getProvince = provinceId => {
+        if (!userAreasMap[provinceId]) {
+          const province = { ...allAreasMap[provinceId], children: [], disabled: true }
+          userAreasMap[provinceId] = province
+          userAreas.push(province)
+        }
+        return userAreasMap[provinceId]
+      }
+
+      const getCity = (provinceId, cityId) => {
+        if (!userAreasMap[cityId]) {
+          const city = { ...allAreasMap[cityId], children: [], disabled: true }
+          userAreasMap[cityId] = city
+          const province = getProvince(provinceId)
+          province.children.push(city)
+        }
+        return userAreasMap[cityId]
+      }
+
+      for (const area of userArea) {
+        if (area.length === 1) {
+          const province = allAreasMap[area[0]]
+          userAreas.push(province)
+        } else if (area.length === 2) {
+          const city = allAreasMap[area[1]]
+          const province = getProvince(area[0])
+          province.children.push(city)
+        } else if (area.length === 3) {
+          const county = allAreasMap[area[2]]
+          const city = getCity(area[0], area[1])
+          city.children.push(county)
+        }
+      }
+
+      return userAreas
     }
   },
   mounted() {},
