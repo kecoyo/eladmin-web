c34691de819349a1b6876e9824274c6c5545d732
杨可可 <yangkeke@dmtop1.com>
2023-12-20 14:26:27 +0800

    babel: 添加新语法支持

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-const plugins = ['@vue/babel-plugin-transform-vue-jsx']
+const plugins = ['@vue/babel-plugin-transform-vue-jsx', '@babel/plugin-proposal-nullish-coalescing-operator', '@babel/plugin-proposal-optional-chaining']
 // 生产环境移除console
 if (process.env.NODE_ENV === 'production') {
   plugins.push('transform-remove-console')
